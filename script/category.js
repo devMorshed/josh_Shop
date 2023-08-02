fee9752a27aabba1dff585a7f32df31989f1830c
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-08-02 18:08:16 +0600

    dropdown added

--- script/category.js
+++ script/category.js
@@ -4,16 +4,19 @@ document.getElementById("cetegory-button").addEventListener('click', function(e)
 
     const faDown = document.getElementById("down-arrow");
     const faup = document.getElementById("up-arrow");
+    const category_ul = document.getElementById('category-lists');
 
 
     if(isOpen){
         faDown.style.display = "none"
         faup.style.display = "block"
+        category_ul.style.height = "240px"
         console.log('up');
     }
     else{
         faDown.style.display = "block"
         faup.style.display = "none"
+        category_ul.style.height = 0
         console.log('down');
     }
 
